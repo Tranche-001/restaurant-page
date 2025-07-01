36130a2a5e6f446c7a4ca07a5a52e49182444dd9
Tranche-001 <tranche100@gmail.com>
2025-07-01 18:05:51 -0300

    add menu button functionality and page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import "./style.css"
-// import { firstSection, secondSection } from "./home";
-// const content = document.querySelector(".content");
-// content.appendChild(firstSection);
-// content.appendChild(secondSection);
+import { firstSection, secondSection } from "./home";
+import { recipePage } from "./recipes";
+const content = document.querySelector(".content");
+
+
+const buttonHome = document.querySelector(".home");
+buttonHome.addEventListener("click", () => {
+  content.innerHTML = "";
+  content.appendChild(firstSection);
+  content.appendChild(secondSection);
+})
+
+const buttonMenu = document.querySelector(".menu");
+buttonMenu.addEventListener("click", () => {
+  content.innerHTML = "";
+  content.appendChild(recipePage);
+})
--- src/recipes.js
+++ src/recipes.js
@@ -1,7 +1,15 @@
-const recipeIMG = document.createElement("img");
-const table = document.createElement("table");
-const tableRow = document.createElement("tr");
-const firstIngredient = document.createElement("td");
+export const recipePage = document.createElement("div");
+recipePage.setAttribute("class", "recipe-page");
 
+const RPh1 = document.createElement("h1");
+RPh1.textContent = "CHECKOUT OUR ITEMS!";
+recipePage.appendChild(RPh1);
+
+const recipeIMG = document.createElement("img");
 recipeIMG.src= "https://platform.polygon.com/wp-content/uploads/sites/2/chorus/uploads/chorus_asset/file/24712315/Zelda_Tears_of_the_Kingdom_best_recipes_2.jpg?quality=90&strip=all&crop=0,0,100,100";
+recipePage.appendChild(recipeIMG);
+
+const RPh12 = document.createElement("h1");
+RPh12.textContent = "AND MANY MORE!";
+recipePage.appendChild(RPh12);
 
